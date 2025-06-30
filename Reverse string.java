672d9158817d0b2c737d7cab2fa588626eb01c2f
Kambhampati Sri Ram <sriramkambhampati2000@gmail.com>
2025-06-30 10:47:33 +0530

    Create Reverse string.java

---
+++ Reverse string.java
@@ -0,0 +1,7 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "hello", rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) rev += str.charAt(i);
+        System.out.println("Reversed: " + rev);
+    }
+}
